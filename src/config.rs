6f1b0ed6f0507ef2f7650ce6eb9e737385c40946
cercky <cercky@qq.com>
2025-04-09 15:03:21 +0800

    Update config.rs
    修改配置

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.ctpworld.net"];
+pub const RS_PUB_KEY: &str = "WAbp9atTGWPi7c4Rw+zzJHFDrreiL7EEOwxI5L6ZeH0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
